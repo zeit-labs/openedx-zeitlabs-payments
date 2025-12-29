dd5aeeab775a6f9fba1c0bd3bfcc9381a4ebf04f
Tehreem Sadat <ts.tehreem@gmail.com>
2025-12-29 16:16:05 +0500

    fix: move basic styling to plugin

--- zeitlabs_payments/helpers.py
+++ zeitlabs_payments/helpers.py
@@ -2,11 +2,17 @@
 
 from __future__ import annotations
 
+import qrcode
+import qrcode.image.svg
+from crum import get_current_request
 import logging
 import re
 from dataclasses import dataclass, field
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from typing import Any, Optional
 from urllib.parse import urljoin
+
 
 from common.djangoapps.course_modes.models import CourseMode
 from common.djangoapps.student.models import (
@@ -380,3 +386,28 @@ def check_duplicate_cart_with_item(
         raise DuplicateCartError(
             f'Duplicate cart found ID: {duplicate_cart.id}, state: {status}.'
         )
+
+
+def generate_invoice_qr_code(invoice_number: str) -> str:
+    """
+    Generate a QR code in SVG format for the checkout receipt page.
+
+    The function creates a checkout receipt URL that includes the given order number
+    as a query parameter. The QR code is generated for this URL and returned as an SVG image.
+
+    :params order_number (int): The order number to include in the checkout receipt URL.
+    :returns: str: A QR code in SVG format, rendered as a string.
+    """
+    receipt_url = reverse(
+        'zeitlabs_payments:invoice',
+        args=[invoice_number]
+    )
+    request = get_current_request()
+    url = request.build_absolute_uri(receipt_url)
+
+    qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image()
+
+    return mark_safe(img.to_string(encoding='unicode'))
